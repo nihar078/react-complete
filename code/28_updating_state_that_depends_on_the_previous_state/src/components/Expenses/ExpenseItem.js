a5374cade23c48775a2058193a463f659b2a7f94
nihar078 <nihar078@gmail.com>
2023-05-24 18:44:08 +0530

    28_updating_state_that_dependsonthe_previousstate

---
+++ code/28_updating_state_that_depends_on_the_previous_state/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from '../UI/Card';
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+   
+  const [title, setTitle] = useState(props.title)
+  const [amount, setAmount] = useState(props.amount)
+  console.log("ExpenseItem evaluated by React")
+
+  function clickHandler(){
+    setTitle('Updated!');
+    console.log(title);
+  }
+  const updateAmount = () =>{
+    setAmount(100);
+    console.log(amount)
+  }
+
+  return (
+    <Card className="expense-item"> 
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div>{props.locationOFExpenditure}</div>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick = {updateAmount}>Change Expense</button>
+    </Card>
+  );
+}
+export default ExpenseItem;
