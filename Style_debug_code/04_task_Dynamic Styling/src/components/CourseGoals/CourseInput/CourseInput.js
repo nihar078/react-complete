46876db5c509f0aaa56cc862f1a6603dd5f2005e
nihar078 <nihar078@gmail.com>
2023-06-01 16:38:43 +0530

    04_task_Dynamic Styling

---
+++ Style_debug_code/04_task_Dynamic Styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [isClicked, setIsClicked] = useState(false)
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    if(event.target.value.trim().length === 0){
+      setIsClicked(false)
+    }
+    if(event.target.value.trim().length === 1){
+      setIsClicked(true)
+    }
+
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <div className={`form-button-control ${!isClicked ? "notcliked" : ""}`}>
+        <Button type="submit">Add Goal</Button>
+      </div>
+    </form>
+  );
+};
+
+export default CourseInput;
