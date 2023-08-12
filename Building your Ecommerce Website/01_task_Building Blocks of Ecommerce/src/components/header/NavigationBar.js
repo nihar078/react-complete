6447a931fb8c5bff325b0fd22c4aecff14377e1a
nihar078 <nihar078@gmail.com>
2023-08-12 17:01:45 +0530

    Final_complete_some work in navigation bar

--- Building your Ecommerce Website/01_task_Building Blocks of Ecommerce/src/components/header/NavigationBar.js
+++ Building your Ecommerce Website/01_task_Building Blocks of Ecommerce/src/components/header/NavigationBar.js
@@ -13,83 +13,88 @@ const NavigationBar = (props) => {
   };
   return (
     <div>
-      <Navbar bg="dark" expand="lg" variant="dark" className="p-1">
+      <Navbar bg="dark" variant="dark" className="p-1">
         <Container>
           <Nav className="m-auto">
-          <NavLink
-            to="/home"
-            className=" mx-3 px-2"
-            style={{
-              color: "white",
-              fontFamily: "serif",
-              fontSize: "18px",
-              textDecoration: "none",
-            }}
-          >
-            HOME
-          </NavLink>
-          <NavLink
-            to="/store"
-            className=" mx-3 px-2"
-            style={{
-              color: "white",
-              fontFamily: "serif",
-              fontSize: "18px",
-              textDecoration: "none",
-            }}
-          >
-            STORE
-          </NavLink>
-          <NavLink
-            to="/about"
-            className="mx-3 px-2"
-            style={{
-              color: "white",
-              fontFamily: "serif",
-              fontSize: "18px",
-              textDecoration: "none",
-            }}
-          >
-            ABOUT
-          </NavLink>
-          <NavLink
-            to="/auth"
-            className="mx-3 px-2"
-            style={{
-              color: "white",
-              fontFamily: "serif",
-              fontSize: "18px",
-              textDecoration: "none",
-            }}
-          >
-            {!authCtx.isLoggedIn && "Login"}
-            {authCtx.isLoggedIn && (
-              <Button
-                type="button"
-                style={{ backgroundColor: "black" }}
-                onClick={logoutHandler}
-              >
-                Logout
-              </Button>
-            )}
-          </NavLink>
-          <NavLink
-            to="/contact"
-            className="mx-3 px-2"
-            style={{
-              color: "white",
-              fontFamily: "serif",
-              fontSize: "18px",
-              textDecoration: "none",
-            }}
-          >
-            CONTACT US
-          </NavLink>
+            <NavLink
+              to="/home"
+              className=" mx-3 px-2"
+              style={{
+                color: "white",
+                fontFamily: "serif",
+                fontSize: "18px",
+                textDecoration: "none",
+              }}
+            >
+              HOME
+            </NavLink>
+            <NavLink
+              to="/store"
+              className=" mx-3 px-2"
+              style={{
+                color: "white",
+                fontFamily: "serif",
+                fontSize: "18px",
+                textDecoration: "none",
+              }}
+            >
+              STORE
+            </NavLink>
+            <NavLink
+              to="/about"
+              className="mx-3 px-2"
+              style={{
+                color: "white",
+                fontFamily: "serif",
+                fontSize: "18px",
+                textDecoration: "none",
+              }}
+            >
+              ABOUT
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className="mx-3 px-2"
+              style={{
+                color: "white",
+                fontFamily: "serif",
+                fontSize: "18px",
+                textDecoration: "none",
+              }}
+            >
+              CONTACT US
+            </NavLink>
           </Nav>
-          {location.pathname === "/store" && (
-            <CartButton onOpen={props.onOpenCart} />
-          )}
         </Container>
+        <NavLink
+          to="/auth"
+          className="mx-3"
+          style={{
+            color: "white",
+            fontFamily: "serif",
+            fontSize: "18px",
+            textDecoration: "none",
+          }}
+        >
+          {!authCtx.isLoggedIn && "Login"}
+        </NavLink>
+        {authCtx.isLoggedIn && (
+          <Button
+            className="mx-1"
+            type="button"
+            style={{
+              backgroundColor: "black",
+              fontSize: "16px",
+              fontWeight: "500",
+            }}
+            onClick={logoutHandler}
+          >
+            Logout
+          </Button>
+        )}
+        {location.pathname === "/store" && (
+          <CartButton onOpen={props.onOpenCart} />
+        )}
       </Navbar>
     </div>
   );
