9639e1a9efce44863e662063ccf0b37d80769e40
nihar078 <nihar078@gmail.com>
2023-12-17 13:08:37 +0530

    task_01_Signup Screen

---
+++ Mail Box Client/mail box client/src/components/Auth/AuthForm.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import { Alert, Button, Container, Form } from "react-bootstrap";
+
+import "./AuthForm.css";
+
+const AuthForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+  const passwordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const confirmPasswordHandler = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const formSubmitHandler = async (event) => {
+    event.preventDefault();
+    if (confirmPassword !== password && confirmPassword.length > 0) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAnUORMfzKjfXTz2B41LKokHy_ks37ANlI`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if(response.ok){
+        const data = await response.json()
+        console.log("User has successfully signed up", data)
+        alert("successfuly signup.. try to login");
+    }
+    else{
+        const data = await response.json()
+        alert(data.error.message)
+    }
+
+    const user = {
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    console.log(user);
+  };
+
+  return (
+    <Container>
+      <Form className="auth" onSubmit={formSubmitHandler}>
+        <h2>SignUp</h2>
+        <Form.Group className="control" controlId="formGroupEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            required
+            value={email}
+            onChange={emailHandler}
+          />
+        </Form.Group>
+        <Form.Group className="control" controlId="formGroupPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            value={password}
+            onChange={passwordHandler}
+          />
+        </Form.Group>
+        <Form.Group className="control" controlId="fromGroupConfirmPassword">
+          <Form.Label>Confirm Password:</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            value={confirmPassword}
+            onChange={confirmPasswordHandler}
+          />
+        </Form.Group>
+        {showAlert && (
+          <Alert variant="danger">
+            Confirm Password doesn't match, please try again!
+          </Alert>
+        )}
+        <div className="actions">
+          <Button type="submit" variant="primary">
+            SignUp
+          </Button>
+        </div>
+      </Form>
+      <div className="actions">
+        <Button variant="light" className="toggle">
+          Have an account?Login
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+export default AuthForm;
